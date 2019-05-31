e73a52a7d1bef98e92f28ecdfe1309a483f40e46
KrishnaGadde-Canwill <50667202+KrishnaGadde-Canwill@users.noreply.github.com>
2019-05-31 20:14:34 +0530

    Changed MimeTypeforFile()  function which uses mimetype library
    This  MimeTypeforFile()  (which uses mimetype library) gives wrong MimeType if the file extension is changed. So I used magic library to give the right mimetype for a file even if the file extension is changes

--- Mimetypeex.py
+++ Mimetypeex.py
@@ -2,13 +2,31 @@ import mimetypes, os
 from mimetypes import guess_extension
 import requests
 
-def MimeTypeForFile(filename):
-    """
-    :param filename: input file name
-    :return: returns the mimetype
-    """
-    type = mimetypes.MimeTypes().guess_type(filename)[0]
-    return  type
+# This below function gives wrong MimeType if the file extension is changed. So I used magic library
+# def MimeTypeForFile(filename):
+#     """
+#     :param filename: input file name
+#     :return: returns the mimetype
+#     """
+#     type = mimetypes.MimeTypes().guess_type(filename)[0]
+#     print(type)
+#     return  typeimeTypes().guess_type(filename)[0]
+#     return  type
+
+def MimeTypeforFile(fname):
+
+    xx = magic.from_file(fname)
+    tx = magic.Magic(mime = True)
+    mime = tx.from_file(fname)
+    print(mime)
+    print(xx)
+
+
+
+
+MimeTypeforFile('14.py')
+
+
 
 
 def ExtnForMimeType(mtype):
